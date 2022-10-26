208f80495198f540c85d9d2632b4be9541fb7214
Martin Procházka <juniwalk@outlook.cz>
2022-10-26 16:35:23 +0200

    Added custom ArrayInput; See #2

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -7,9 +7,9 @@
 
 namespace JuniWalk\Utils\Console;
 
+use JuniWalk\Utils\Console\Input\ArrayInput;
 use JuniWalk\Utils\Exceptions\CommandFailedException;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -71,7 +71,7 @@ abstract class AbstractCommand extends Command
 		foreach ($commandList as $commandName => $arguments) {
 			$command = $application->get($commandName);
 
-			$input = new ArrayInput($arguments, $command->getDefinition());
+			$input = new ArrayInput($command->getDefinition(), $arguments);
 			$input->setInteractive(false);
 
 			if ($callback && $callback($command, $input) === false) {
---
+++ src/Console/Input/ArrayInput.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2022
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Utils\Console\Input;
+
+use Symfony\Component\Console\Input\ArrayInput as BaseInput;
+use Symfony\Component\Console\Input\InputDefinition;
+
+class ArrayInput extends BaseInput
+{
+	public function __construct(InputDefinition $definition, array $params = [])
+	{
+		parent::__construct($params, $definition);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function bind(InputDefinition $definition)
+	{
+		if ($this->definition <> $definition) {
+			return parent::bind($definition);
+		}
+
+		$this->parse();
+	}
+}

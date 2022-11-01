903c44df49a1231cbf58d00879049adc9ce6bd7e
Martin Procházka <juniwalk@outlook.cz>
2022-11-01 08:58:27 +0100

    Switched Json::literal to use anonymous class

--- src/Json.php
+++ src/Json.php
@@ -12,30 +12,28 @@ use Nette\Utils\JsonException;
 use Nette\IOException;
 use Stringable;
 
-final class Json implements Stringable
+final class Json
 {
-	private const PATTERN = '/(\"code\:([\/=+0-9a-z]+)\")/iU';
-	private const LITERAL = 'code:';
+	public const PATTERN = '/(\"code\:([\/=+0-9a-z]+)\")/iU';
+	public const LITERAL = 'code:';
 
 	public const FORCE_ARRAY = NetteJson::FORCE_ARRAY;
 	public const PRETTY = NetteJson::PRETTY;
 	public const ESCAPE_UNICODE = NetteJson::ESCAPE_UNICODE;
 
 
-	private function __construct(
-		private readonly string $code
-	) {}
-
-
-	public function __toString(): string
+	public static function literal(string $code): object
 	{
-		return static::LITERAL.base64_encode($this->code);
-	}
+		return new class($code) implements Stringable {
+			public function __construct(
+				private readonly string $code
+			) {}
 
-
-	public static function literal(string $code): static
-	{
-		return new static($code);
+			public function __toString(): string
+			{
+				return Json::LITERAL.base64_encode($this->code);
+			}
+		};
 	}
 
 

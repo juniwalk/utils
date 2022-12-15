f65d062cb9d5075bc833bc5583db3a8dddb42224
Martin Procházka <juniwalk@outlook.cz>
2022-12-15 14:59:54 +0100

    Simplified list of available colors

--- src/Enums/Color.php
+++ src/Enums/Color.php
@@ -11,17 +11,12 @@ enum Color: string implements LabeledEnum
 {
 	use Traits\Labeled;
 
-	/** Basic Bootstrap colors */
 	case Primary = 'primary';
 	case Secondary = 'secondary';
 	case Info = 'info';
 	case Success = 'success';
 	case Warning = 'warning';
 	case Danger = 'danger';
-	case Light = 'light';
-	case Dark = 'dark';
-
-	/** Extended AdminLTE colors */
 	case Indigo = 'indigo';
 	case LightBlue = 'lightblue';
 	case Navy = 'navy';
@@ -33,11 +28,7 @@ enum Color: string implements LabeledEnum
 	case Lime = 'lime';
 	case Teal = 'teal';
 	case Olive = 'olive';
-
-	/** Black & White AdminLTE colors */
-	case Black = 'black';
 	case GrayDark = 'gray-dark';
-	case Gray = 'gray';
 
 
 	public function label(): string
@@ -54,7 +45,7 @@ enum Color: string implements LabeledEnum
 
 	public function icon(): ?string
 	{
-		return 'fas fa-droplet';
+		return 'fa-solid fa-droplet';
 	}
 
 
@@ -71,17 +62,12 @@ enum Color: string implements LabeledEnum
 	public function hex(): string
 	{
 		return match($this) {
-			/** Basic Bootstrap colors */
 			self::Primary => '#007bff',
 			self::Secondary => '#6c757d',
 			self::Info => '#17a2b8',
 			self::Success => '#28a745',
 			self::Warning => '#ffc107',
 			self::Danger => '#dc3545',
-			self::Light => '#f8f9fa',
-			self::Dark => '#343a40',
-
-			/** Extended AdminLTE colors */
 			self::Indigo => '#6610f2',
 			self::LightBlue => '#3c8dbc',
 			self::Navy => '#001f3f',
@@ -93,11 +79,7 @@ enum Color: string implements LabeledEnum
 			self::Lime => '#01ff70',
 			self::Teal => '#20c997',
 			self::Olive => '#3d9970',
-		
-			/** Black & White AdminLTE colors */
-			self::Black => '#000000',
 			self::GrayDark => '#343a40',
-			self::Gray => '#6c757d',
 		};
 	}
 
@@ -105,12 +87,9 @@ enum Color: string implements LabeledEnum
 	public function foreground(): string
 	{
 		return match ($this) {
-			self::Warning => self::Dark->hex(),
-			self::Light => self::Dark->hex(),
-			self::Teal => self::Dark->hex(),
-			self::Lime => self::Dark->hex(),
-			self::Orange => self::Dark->hex(),
-			default => self::Light->hex(),
+			self::Warning, self::Teal,
+			self::Lime, self::Orange => '#343a40',
+			default => '#f8f9fa',
 		};
 	}
 
@@ -118,9 +97,9 @@ enum Color: string implements LabeledEnum
 	private function isBasicColor(): bool
 	{
 		return match($this) {
-			self::Primary, self::Secondary, self::Info,
-			self::Success, self::Warning, self::Danger,
-			self::Light, self::Dark => true,
+			self::Primary, self::Secondary,
+			self::Info, self::Success,
+			self::Warning, self::Danger => true,
 			default => false,
 		};
 	}

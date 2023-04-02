e070e593c86809d9cdd0db4879ecc0215b64c31f
Martin Procházka <juniwalk@outlook.cz>
2023-04-02 12:10:08 +0200

    Fixed use of deprecated LabeledEnum to new one

--- src/UI/DataGrids/AbstractGrid.php
+++ src/UI/DataGrids/AbstractGrid.php
@@ -8,7 +8,7 @@
 namespace JuniWalk\Utils\UI\DataGrids;
 
 use JuniWalk\Utils\Enums\Color;
-use JuniWalk\Utils\Enums\LabeledEnum;
+use JuniWalk\Utils\Enums\Interfaces\LabeledEnum;
 use JuniWalk\Utils\Html;
 use Nette\Application\UI\Control;
 use Nette\Localization\Translator;

ae7bb2b3229955764271acaccba38edea5d190d5
juniwalk <juniwalk@outlook.cz>
2024-05-02 10:16:28 +0200

    Returned modal handling back from components

---
+++ src/Traits/Modals.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2023
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Components\Traits;
+
+use JuniWalk\Utils\Interfaces\EventHandler;
+use JuniWalk\Utils\Interfaces\Modal;
+use Nette\InvalidArgumentException;
+
+trait Modals
+{
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	public function openModal(Modal|string $modal, array $params = []): void
+	{
+		if (is_string($modal) && !str_starts_with($modal, '#')) {
+			$modal = $this->getComponent($modal, true);
+		}
+
+		if ($modal instanceof Modal) {
+			if ($modal instanceof EventHandler && $modal->isWatched('render')) {
+				$modal->when('render', fn($m, $t) => $t->setParameters($params));
+				$params = [];
+			}
+
+			$modal->setModalOpen(true);
+			$modal = '#'.$modal->getName();
+		}
+
+		$template = $this->getTemplate();
+		$template->add('openModal', $modal);
+		$template->setParameters($params);
+
+		$this->redrawControl('modals');
+		$this->redirect('this');
+	}
+}
